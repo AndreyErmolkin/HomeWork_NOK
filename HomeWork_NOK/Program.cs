cb3e9606cd7646ebb9fa20325548c3138c7ba9fe
AndreyErmolkin <78263246+AndreyErmolkin@users.noreply.github.com>
2021-03-04 19:39:03 +0300

    Добавьте файлы проекта.

---
+++ HomeWork_NOK/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HomeWork_NOK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Число N=");
+            int N = int.Parse(Console.ReadLine());
+            Console.WriteLine("Число M=");
+            int M = int.Parse(Console.ReadLine());
+
+            int i;
+
+            for (i = 0; i < (N * M) + 1; i++)
+            {
+                if (i % N == 0 && i % M == 0)
+                {
+                    int NOK = i;
+                    if (i!=0)
+                    {
+                        Console.WriteLine("Наименьшее общее кратное = " + i);
+                    }    
+
+                }
+            }
+        }
+    }
+}
